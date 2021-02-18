acc0610fb5d74f1cd18789c9ef02f916ac02e674
kts of kettek (POWERQWACK) <kts@kettek.net>
2021-02-18 15:55:02 -0800

    Remove externalize; bump version

--- plugin.js
+++ plugin.js
@@ -5,15 +5,11 @@
  * @param {object} pluginOptions The plugin options.
  * @param {string[]} [pluginOptions.modules=[]] An array of module names to match against.
  * @param {string[]} [pluginOptions.types=['.js']] An array of file extensions to match against, including the leading dot.
- * @param {boolean} [pluginOptions.externalize=true] Whether or not the passed modules should be added to snowpack's packageOptions.external array.
  * @returns {object} The snowpack plugin.
  */
 module.exports = function selectiveImportToRequire(snowpackConfig, pluginOptions) {
   const regexp = new RegExp("^import (?<variableName>((?!from).|\n)*?) from (?<moduleName>(?:'|\")(?:"+(pluginOptions.modules||[]).join('|')+")(?:'|\")).*$", "gm")
   const filetypes = pluginOptions.types || ['.js']
-  if (pluginOptions.externalize === undefined || pluginOptions.externalize === true) {
-    snowpackConfig.packageOptions.external = [...snowpackConfig.packageOptions.external, ...pluginOptions.modules]
-  }
   return {
     name: 'snowpack-plugin-selective-import-to-require',
     async transform({ id, contents, isDev, fileExt }) {
